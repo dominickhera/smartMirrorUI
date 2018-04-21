7f6814f6fa7a1fb5752ac967a8383a36888132b7
dominickhera <dominickhera@me.com>
2018-04-21 16:44:39 -0400

    weather not being recognised for some reasion

--- mirrorInterface.py
+++ mirrorInterface.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python
 
-from kivy.app import App
-from kivy.uix.button import Button
+from Tkinter import *
+import tkMessageBox 
+import time
+from datetime import date
+import calendar
+# from weather import Weather
+# weather = Weather(unit=Unit.CELSIUS)
 
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello World')
+root = Tk()
 
-TestApp().run()
+canavasThing = Canvas(root, height = 500, width = 750) 
+timeVar = StringVar()
+timeLabel = Label( root, textvariable=timeVar, relief=FLAT)
+# timeLabel.Font(family="Times", size=10, weight=tkFont.BOLD)
+timeVar.set(time.strftime("%I:%M:%S"))
+timeLabel.pack()
+
+dateVar = StringVar()
+dateLabel = Label( root, textvariable=dateVar, relief=FLAT)
+dateVar.set(time.strftime("%d/%m/%Y"))
+dateLabel.pack()
+
+my_date = date.today()
+dayVar = StringVar()
+dayLabel = Label( root, textvariable=dayVar, relief=FLAT)
+dayVar.set(calendar.day_name[my_date.weekday()])
+dayLabel.pack()
+
+# location = weather.lookup_by_location('guelph')
+# condition = location.condition()
+# weatherVar = StringVar()
+# weatherLabel = Label( root, textvariable=weatherVar, relief=FLAT)
+# weatherVar.set(condition.text())
+# weatherLabel.pack()
+
+root.title("Smart Mirror GUI")
+canavasThing.pack()
+root.mainloop()
